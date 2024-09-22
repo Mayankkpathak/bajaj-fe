75734b1dfe8d409e76dc2ade39d4387084718d86
Mayank Pathak <100159883+Mayankkpathak@users.noreply.github.com>
2024-09-22 17:30:55 +0530

    build successfull

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-black">
       <div className="text-6xl font-extrabold text-white mb-8">
-        RA2111003011823
+        RA2111003011845
       </div>
-      <div className="w-full max-w-lg p-4 bg-white rounded-lg shadow-lg">
+      <div className="w-full max-w-lg p-4 bg-[#ffffff50] backdrop-blur-xl rounded-lg shadow-lg">
         <ApiForm />
       </div>
     </main>
