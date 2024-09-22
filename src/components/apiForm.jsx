1140b1210d940e0ec706a15ab1794844bfa14830
Mayank Pathak <100159883+Mayankkpathak@users.noreply.github.com>
2024-09-22 17:51:32 +0530

    build successfull

--- src/components/apiForm.jsx
+++ src/components/apiForm.jsx
@@ -138,7 +138,7 @@ const ApiForm = () => {
     const formattedData = formatData(data, dataFormat);
 
     try {
-      const res = await fetch("http://localhost:4000/bfhl", {
+      const res = await fetch("https://bajaj.humbleayush30.workers.dev/bfhl", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
