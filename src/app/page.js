f73beb5ed3edcfa2fea2a6825588720a11f99e11
Mayank Pathak <100159883+Mayankkpathak@users.noreply.github.com>
2024-09-22 17:21:57 +0530

    k

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import ApiForm from "@/components/apiForm";
+// import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-black">
+      <div className="text-6xl font-extrabold text-white mb-8">
+        RA2111003011823
+      </div>
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg shadow-lg">
+        <ApiForm />
+      </div>
+    </main>
+  );
+}
